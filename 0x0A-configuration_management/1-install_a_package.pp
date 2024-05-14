95658e9e3f3d7fdf757983ddadc6f08c1b5c85d7
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2024-05-13 18:21:16 -0700

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# installs flask from pip3 using Puppet
+# Installs Flask version 2.1.0 from pip3 using Puppet
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure         => '2.1.0',
+  provider       => 'pip3',
+  install_options => ['--user', '--no-warn-script-location'],
 }
