6e12922b1b9676d9b93f42734595803e96413f39
Cherifi hiba <cherifi.hiba.2019@gmail.com>
2025-01-19 16:51:30 -0800

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,15 @@
-# Install Flask with Puppet
+# This Puppet manifest installs Flask version 2.1.0 and Werkzeug version 2.1.1 using pip3 on Ubuntu 20.04 LTS.
 
-package { 'python3.8':
-  ensure   => '3.8.10',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path    => ['/usr/bin', '/bin'],
+  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
+  require => Exec['install_flask'],
 }
 
-package { 'Werkzeug':
-  ensure  => '2.1.1',
-  provider => 'pip3',
-  require => Package['Flask'],
-}
